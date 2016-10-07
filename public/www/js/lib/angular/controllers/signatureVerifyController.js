f69c1c4965fecabe12d7c460b7360c5da9c32cf0
Duraimurugan Govindaraj <durai145@live.in>
2016-10-07 05:48:54 -0400

    angularejs

---
+++ public/www/js/lib/angular/controllers/signatureVerifyController.js
@@ -0,0 +1,87 @@
+define([],
+function()
+{
+
+	return [ '$scope' , 'toaster','signatureVerifyService','$state','$window',function($scope,toaster,signatureVerifyService,$state, $window){
+
+
+
+
+
+$scope.Submit=function()
+{
+
+  toaster.pop('success', "title", "text");
+}
+
+$scope.uploadFile=function()
+{
+
+  alert(" I am here for upload");
+}
+
+   
+
+         $scope.$watch('$viewContentLoaded', function(){
+    //Here your view content is fully loaded !!
+   // 2 alert('on viewContentLoaded watch');
+ 
+  });
+
+       
+
+          angular.element(document).ready(function () {
+   // 1 alert('page loading completed');
+});
+	}];
+	
+});
+
+/*[
+
+ toasterService.pop('success', "title", "text");
+      var url = "/authorize"; 
+      var config =  { 
+          headers: {
+            "x-access-token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJIZWFlcmllIEdTTCIsImF1ZCI6Ind3dy5teXJvb21leHBlbnNlLmNvbSIsImlhdCI6IjYwbXMifQ.G37Yj8ljUjbOf-kSyc4j3-YAlbseb1KET9CMBgbJfaE"
+           ,'Authorization': 'Basic dGVzdDp0ZXN0'
+            ,      'Content-Type': 'application/x-www-form-urlencoded'
+            ,'Access-Control-Allow-Origin': false
+
+            
+        }
+      };
+      var dataJson = 
+      {
+        "email" : "durai145@live.in"
+        ,"password" : "1qaz2wsx"
+        ,"grantType": "password"
+            ,"clientId" : "CLIENTSP"
+            ,"scope" : "GSA"
+            ,"redirectURI" :"http://localhost:5000"
+      };
+
+    /*
+    $.post(url , dataJson , function (resp,status,xhr){
+
+        alert("resp" + status);
+
+    },dataType);
+
+
+  $http.post(url,dataJson,config).then(function (response) 
+    { 
+     // alert("resp");
+      console.log(response);
+      alert(JSON.stringify(response));
+    },function(data){
+
+      if(data.status == 302)
+      {
+
+        alert("Invalid request");
+      }
+     });
+    };
+
+]*/
